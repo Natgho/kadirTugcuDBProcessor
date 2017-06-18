8e6eaba4e5927e01d367aa385701e860dcd06964
natgho-reidin <sbozkir@reidin.com>
2017-06-18 15:43:54 +0300

    Peewee ORM implemented

---
+++ README.md
@@ -0,0 +1,4 @@
+# KadirTugcuCrawlerProcessor  
+This software is written to clear the crawled data.
+
+- KadirTugcuCrawler Url: https://github.com/Natgho/kadirtugcuCrawler-X
---
+++ mysql_datasource.py
@@ -0,0 +1,31 @@
+# Coded by Sezer Yavuzer Bozkır <admin@sezerbozkir.com>
+# Date: 18.06.2017
+
+from peewee import *
+from playhouse.pool import PooledMySQLDatabase
+from config import Config
+
+db = PooledMySQLDatabase(**Config.mysql)
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Articles(BaseModel):
+    id = PrimaryKeyField(db_column="id")
+    url = TextField(db_column="url")
+    publish_date = TextField(db_column="publish_date")
+    category = TextField(db_column="category")
+    question = TextField(db_column="question")
+    answer = TextField(db_column="answer")
+
+    class Meta:
+        db_table = "articles"
+
+
+if __name__ == '__main__':
+    # db.create_table(Articles, safe=True)
+    for icerik in Articles.select().limit(10):
+        print(icerik.category)
